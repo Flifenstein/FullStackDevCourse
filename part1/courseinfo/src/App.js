c8b7326f6866e1cb2ee190e45e4f8896150d9d8c
Frincu Ioana <ioanafrincu@outlook.com>
2023-07-17 20:44:59 +0300

    Exercises 1.3 and 1.4 changes

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,5 +1,6 @@
 const Header = (props) => {
   return(
+    console.log(props),
     <h1>{props.course}</h1>
   )
 }
@@ -29,9 +30,22 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
+  // 1.3 
+  //const course = 'Half Stack application development'
+  // const part1 = {
+  //   name: 'Fundamentals of React',
+  //   exercises: 10
+  // }
+  // const part2 = {
+  //   name: 'Using props to pass data',
+  //   exercises: 7
+  // }
+  // const part3 = {
+  //   name: 'State of a component',
+  //   exercises: 14
+  // }
+  const course = "Half Stack application development"
+  const parts = [
       {
         name: "Fundamentals of React",
         exercise: 10,
@@ -44,14 +58,14 @@ const App = () => {
         name: "State of a component",
         exercise: 14,
       },
-    ],
-  };
+    ]
+  
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
